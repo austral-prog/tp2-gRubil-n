2732ae2b9420459b87a828d97ebcad48861b73cd
Thus spoke Zarathustra <zaratustra.business@gmail.com>
2025-08-28 10:09:52 -0300

    ADA y CHANGE completados

--- change.py
+++ change.py
@@ -1,3 +1,18 @@
 def change():
     expense = 23.75
     money = 100
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+
+    enPesos = int((money-expense) // 1)
+    enCentavos = int(((money-expense) - int(money-expense)) * 100)
+
+    print("\n Vuelto")
+    print("Pesos:")
+    print(enPesos)
+    print("Centavos:")
+    print(enCentavos)
+
+change()
